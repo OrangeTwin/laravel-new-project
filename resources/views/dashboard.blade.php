c9d09ab21d9b18feb9401fed69935c1a292f6b3c
pacificocean777 <oceanpasific1997@gmail.com>
2018-01-11 14:47:31 +0200

    Correcting remark №3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,7 +27,7 @@
                                 <td>{{$post->title}}</td>
                                 <td><a href = "/posts/{{$post->id}}/edit" class = "btn btn-default">Edit</a></td>
                                 <td>
-                                    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                    {!!Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                                         {{Form::hidden('_method', 'DELETE')}}
                                         {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
                                     {!!Form::close()!!}
